f5262fe1ef1963cffca43555478e8903c59f9e7d
lchrisman <44279697+lchrisman@users.noreply.github.com>
2018-10-18 17:42:22 -0700

    Add files via upload

---
+++ Something.js
@@ -0,0 +1,16 @@
+			function myfunction() {
+				document.getElementById("headerElement").innerHTML="Goodbye World!";
+			}
+			function changeBack() {
+				document.getElementById("headerElement").innerHTML="Hello World!";
+			}
+			function postToConsole() {
+				console.log ("Hello from the Console!")
+			}
+			function goToGoogle() {
+				location.href="https://google.com"
+			}
+			function spamMe() {
+				alert("Hello Leah!");
+				console.log("I'm also posting to the console!");
+			}
